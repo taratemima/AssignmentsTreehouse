756fc99e50f962bef91e50cd11080b34d36844d7
taratemima <taedwards@gmail.com>
2015-05-19 11:50:40 -0400

    Adding code from workspace and new code

--- dungeon.py
+++ dungeon.py
@@ -1,23 +1,76 @@
 import random 
 '''Written by Tara Edwards for Treehouse'''
+'''Uses random.choice for two lists of a range between 0 and 2'''
 
-def set_coordinates(coord):
-    pass
+CELLS = [(0,0), (0,1), (0,2),
+        (1,0), (1,1), (1,2),
+        (2,0), (2,1), (2,2)]
 
-def move(coord):
-    pass
+def get_locations():
+  # monster = random
+    monsterX = random.choice(range(0,2))
+    monsterY = random.choice(range(0,2))
+  
+  # door = random
+    doorX = random.choice(range(0,2))
+    doorY = random.choice(range(0,2))
+
+  # start = random
+    startX = random.choice(range(0,2))
+    startY = random.choice(range(0,2))
+
+  # if monster, door, or start are the same, do it again
+    startX, startY = reroll(startX, startY, monsterX, monsterY)
+    startX, startY = reroll(startX, startY, doorX, doorY)
+    doorX, doorY = reroll(monsterX, monsterY, doorX, doorY)
+#No sense getting eaten just before you escape 
+    monster_coord = (monsterX, monsterY)
+    door_coord = (doorX, doorY)
+    start_coord = (startX, startY)
     
-def change_coordinates(coord):
-   pass
+    return monster_coord, door_coord, start_coord 
+  
+  # return monster, door, start
+  
+def reroll(playerX, playerY, otherX, otherY):
+    if playerX == otherX and playerY == otherY:
+        playerX = random.choice(range(0,2))
+        playerY = random.choice(range(0,2))
+    else:
+        return playerX, playerY
 
-def directions():
-   pass
+  
+def move_player(player, move):
+  # Get the player's current location
+  # If move is LEFT, y - 1
+  # If move is RIGHT, y + 1
+  # If move is UP, x - 1
+  # If move is DOWN, x + 1
+  return player
 
-def escape(coord_player, coord_door):
-   pass
 
-def eaten_by_a_grue(coord_player, coord_monster):
-#Ha ha I made a joke about a game that I haven't played yet
-#That is a lie--Kingdom of Loathing has used that shoutout
-#and I played that
+def get_moves(player):
+  MOVES = ['LEFT', 'RIGHT', 'UP', 'DOWN']
+  # if player's y is 0, remove LEFT
+  # if player's x is 0, remove UP
+  # if player's y is 2, remove RIGHT
+  # if player's x is 2, remove DOWN
+  return MOVES
 
+while True:
+  print("Welcome to the dungeon!")
+  print("You're currently in room {}")  # fill in with player position
+  print("You can move {}")  # fill in with available moves
+  print("Enter QUIT to quit")
+  
+  move = input("> ")
+  move = move.upper()
+  
+  if move == 'QUIT':
+    break
+    
+  # If it's a good move, change the player's position
+  # If it's a bad move, don't change anything
+  # If the new player position is the door, they win!
+  # If the new player position is the monster, they lose!
+  # Otherwise, continue
