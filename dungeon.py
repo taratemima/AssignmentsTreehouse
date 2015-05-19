f741cc8bf063a7e90a74bc6bfdf1131949dce02b
taratemima <taedwards@gmail.com>
2015-05-19 11:11:25 -0400

    added dungeon

---
+++ dungeon.py
@@ -0,0 +1,23 @@
+import random 
+'''Written by Tara Edwards for Treehouse'''
+
+def set_coordinates(coord):
+    pass
+
+def move(coord):
+    pass
+    
+def change_coordinates(coord):
+   pass
+
+def directions():
+   pass
+
+def escape(coord_player, coord_door):
+   pass
+
+def eaten_by_a_grue(coord_player, coord_monster):
+#Ha ha I made a joke about a game that I haven't played yet
+#That is a lie--Kingdom of Loathing has used that shoutout
+#and I played that
+
