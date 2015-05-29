7b865fb987d4b805b48489f76863991046bbf3fc
taratemima <taedwards@gmail.com>
2015-05-29 19:52:49 -0400

    regular expressions
    While not tested, I did save the work I did do

--- regex.py
+++ regex.py
@@ -12,3 +12,28 @@ def first_number(data_string):
 def numbers(count_int, count_string):
   return re.search(r'\d' * count_int, count_string)
 
+#contacts = re.search(r'(?P<email>[-\w\d.+]+@[-\w\d.]+),\s(?P<phone>\d{3}-\d{3}-\d{4})', string, re.X|re.M)
+#twitters = re.search(r'@\w+$', string, re.MULTILINE)
+
+
+string = '''Love, Kenneth: 20
+Chalkley, Andrew: 25
+McFarland, Dave: 10
+Kesten, Joy: 22
+Stewart Pinchback, Pinckney Benton: 18'''
+
+players = re.search("""
+    (?P<last_name>\w+?\s*\w*)(?:,\s)
+    (?P<first_name>\w+?\s*\w*)(?:\:\s)
+    (?P<score>\d+)
+""", string, re.I|re.M|re.X)
+
+class Player:
+
+    def __init__(self, last_name, first_name, score):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.score = score
+
+#needs to be tested in the complier
+
